74e0a9cf39c04e7c2f8c3a99bde7ea91cf9d7e0e
Rian <fukriandysisnadi@gmail.com>
2022-03-11 15:43:27 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+
+const notes = [];
+ 
+module.exports = notes;
